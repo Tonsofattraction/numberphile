d3701a825d48e92629151906891fbb866b01e734
noob <noob@yandex-team.ru>
2018-05-07 01:35:05 +0300

    try collatz;

--- collatz.go
+++ collatz.go
@@ -2,23 +2,23 @@ package main
 
 import "fmt"
 
-func collatz(n int16, i int16) {
-	fmt.Printf("%d", n)
+func collatz(n int16, i int16) bool {
 	i++
 	if n == 1 {
 		fmt.Println(i)
-		return
+		return true
 	}
 	if n % 2 == 0 {
-		collatz(n/2, i)
+		return collatz(n/2, i)
 	} else {
-		collatz(3*n+1, i)
+		return collatz(3*n+1, i)
 	}
 }
 
 func main () {
 	var n int16;
 	for n = 2; n < 1000; n++ {
-		collatz(n, 1)
+		fmt.Println(n)
+		collatz(n, 0)
 	}
 }
